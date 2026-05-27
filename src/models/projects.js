73984728c911fa611bcb24c27854a4d58b9ae19a
Gabriel Bastardo <gabitobastardo@gmail.com>
2026-05-27 17:09:13 -0400

    Learning activities W03

--- src/models/projects.js
+++ src/models/projects.js
@@ -22,4 +22,25 @@ const getAllProjects = async () => {
 };
 
 
-export { getAllProjects };
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+        SELECT
+          project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          project_date
+        FROM project
+        WHERE organization_id = $1
+        ORDER BY project_date;
+      `;
+
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+// Export the model functions
+export { getAllProjects, getProjectsByOrganizationId };
